4e8e31f23921e20be174d25ef000e9c4daf6bcbd
Christopher Stenqvist <capodacac@gmail.com>
2013-07-18 11:23:25 -0700

    Swapped index.view && homelink.view

--- app/controllers/blog_entries_controller.rb
+++ app/controllers/blog_entries_controller.rb
@@ -1,7 +1,7 @@
 class BlogEntriesController < ApplicationController
    	def index
    		@blog_all = BlogEntry.all
-	  end
+    end
 
     def show
 		  unless @blog_entry = BlogEntry.find_by_id(params[:id])
@@ -12,13 +12,11 @@ class BlogEntriesController < ApplicationController
     def create
       @blog_entry = BlogEntry.new(params[:blog_entry])
       @blog_entry.save
-      redirect_to blog_entries_homelink_path.to_s 
      end
 
     def destroy
       @del_entry = BlogEntry.find(params[:id])
       @del_entry.destroy
-      redirect_to blog_entries_homelink_path.to_s
     end
 
     def home_link
