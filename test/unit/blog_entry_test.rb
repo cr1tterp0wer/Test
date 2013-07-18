1fc6f14f76cf5c8049a5c32d0d0bf6d6d09aacf9
Christopher Stenqvist <capodacac@gmail.com>
2013-07-18 12:41:53 -0700

    Save wip 2

--- test/unit/blog_entry_test.rb
+++ test/unit/blog_entry_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class BlogEntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#mashify!" do
+  	blog_entry = BlogEntry.new
+  	blog_entry.title = "mash"
+
+    assert_equal "M*A*S*H", blog_entry.mashify!
+
+    blog_entry.title = "chips"
+
+    assert_equal "C*H*I*P*S", blog_entry.mashify!
+  end
 end
