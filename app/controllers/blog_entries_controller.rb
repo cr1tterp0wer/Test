5ef7814688b74b15f4b357b251836e6eaa291288
Christopher Stenqvist <capodacac@gmail.com>
2013-07-18 15:33:46 -0700

    Added filters

--- app/controllers/blog_entries_controller.rb
+++ app/controllers/blog_entries_controller.rb
@@ -1,37 +1,39 @@
 class BlogEntriesController < ApplicationController
-   	def index
-   		@blog_all = BlogEntry.all
-    end
+  before_filter :require_blog_entry, :only => [:show, :destroy]
+  
+ 	def index
+ 		@blog_all = BlogEntry.all
+  end
 
-    def show
-		  unless @blog_entry = BlogEntry.find_by_id(params[:id])
-        render :inline => "Not found", :status => 404
-      end
-    end
+  def show
+  end
 
-    def new
-      @blog_entry = BlogEntry.new
-    end
+  def new
+    @blog_entry = BlogEntry.new
+  end
 
 
-    def create
-      @blog_entry = BlogEntry.new(params[:blog_entry])
-      @blog_entry.save
-    
-      redirect_to blog_entries_path
-    end
+  def create
+    @blog_entry = BlogEntry.new(params[:blog_entry])
+    @blog_entry.save
+  
+    redirect_to blog_entries_path
+  end
 
-    def destroy
-      if @del_entry = BlogEntry.find_by_id(params[:id])
-        @del_entry.destroy
-        redirect_to blog_entries_path
-      else
-        render :inline => "Not found", :status => 404
-      end  
-    end
+  def destroy
+    @blog_entry.destroy
+    redirect_to blog_entries_path
+  end
 
-    def home_link
-      @blog_all = BlogEntry.all
+  def home_link
+    @blog_all = BlogEntry.all
+  end
+
+protected
+  def require_blog_entry
+    unless @blog_entry = BlogEntry.find_by_id(params[:id])
+      render :inline => 'Not found', :status => 404 and return false
     end
+  end
 
 end
