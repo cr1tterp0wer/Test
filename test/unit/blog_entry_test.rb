a573b70b10853cce2985ca39df87cd8124271b21
Christopher Stenqvist <capodacac@gmail.com>
2013-07-19 15:47:21 -0700

    mashify-dashify test complete

--- test/unit/blog_entry_test.rb
+++ test/unit/blog_entry_test.rb
@@ -9,20 +9,31 @@ class BlogEntryTest < ActiveSupport::TestCase
 
     should "look like the mash logo" do
       @blog_entry.mashify_title = true;
+
       assert_equal "M*A*S*H", @blog_entry.mashify!
     end
 
     should "replace * with !" do
       @blog_entry.body = "sh*t"
       @blog_entry.mashify_title = true;
+
       assert_equal "S*H*!*T", @blog_entry.mashify!
     end
 
     should "remove whitespace" do
       @blog_entry.body = "        mash         "
       @blog_entry.mashify_title = true
+
       assert_equal "M*A*S*H", @blog_entry.mashify!
     end
+
+    should "remove all *'s when not checked" do
+      @blog_entry.body = "M*A*S*H"
+      @blog_entry.mashify_title = false
+
+      assert_equal "mash", @blog_entry.mashify!
+    end
+  end
 
   context '#dashify!' do
     setup do
@@ -36,10 +47,7 @@ class BlogEntryTest < ActiveSupport::TestCase
 
   	  assert_equal "F-O-O", @blog_entry.dashify!
     end
-    
-  end
-end 
-=begin
+
     should "Remove all -'s When not checked" do
       @blog_entry.body = "F-O-O"
       @blog_entry.dashify_title = false
@@ -47,19 +55,40 @@ end
       assert_equal "foo", @blog_entry.dashify!
     end
   end
-end
-
-=begin
-      # blog_entry.title = 'f-o'
-      #  assert_equal "F-*-O", blog_entry.dashify!
-
-
 
   context '#mash_dash!' do
-    blog_entry = BlogEntry.new
+    setup do
+      @blog_entry = BlogEntry.new
+    end
 
-    blog_entry.body = "foo"
-    assert_equal "F*-*O*-*O", blog_entry.mash_dash!
+    should "call mashify! and dashify!" do 
+      @blog_entry.body = "foo bar"
+      @blog_entry.dashify_title = true;
+      @blog_entry.mashify_title = true;
+
+      assert_equal "F*-*O*-*O*-* *-*B*-*A*-*R", @blog_entry.mash_dash!
+    end
+
+    should "remove both *'s and -'s" do 
+      @blog_entry.body = "F*-*O*-*O*-* *-*B*-*A*-*R"
+      @blog_entry.dashify_title = false;
+      @blog_entry.mashify_title = false;
+
+      assert_equal "foo bar", @blog_entry.mash_dash!
+    end
+
+
+
+
+
   end
-=end
+end
+
+
+
+
+
+
+
+
 
