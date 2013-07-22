197600b840b3573308532427072f0267d5cfea51
Christopher Stenqvist <capodacac@gmail.com>
2013-07-22 12:11:30 -0700

    user model created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130719180646) do
+ActiveRecord::Schema.define(:version => 20130722190940) do
 
   create_table "blog_entries", :force => true do |t|
     t.text     "title"
@@ -31,5 +31,12 @@ ActiveRecord::Schema.define(:version => 20130719180646) do
   end
 
   add_index "comments", ["blog_entry_id"], :name => "index_comments_on_blog_entry_id"
+
+  create_table "users", :force => true do |t|
+    t.string   "login"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
 
 end
