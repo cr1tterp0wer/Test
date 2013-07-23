0cf899367574b054b7420400104cf5fb05150a51
Christopher Stenqvist <capodacac@gmail.com>
2013-07-22 17:54:40 -0700

    refactored tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
- # before_filter :require_params, only:[:create]
-
   def index
   end
 
@@ -8,20 +6,15 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
   end
 
-  def create
+  def create  
     @user = User.new(params[:user])
-    @user.save!
-
-    @user_all = User.all
-  end
-
-protected
-  def require_params
-    if User.find_by_id(:id).login
-      render :inline => 'Login/Password cannot remain blank!', :status => 404 and return false
+    @users = User.all
+    unless @user.save
+      render :action => :new and return
     end
   end
 
+protected
   def user_require
     params.require(:user).permit(:login, :password_digest)
   end
--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,28 +1,28 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-    context "#new" do
-      should "render ok" do
-        post :new,  :user => {login: 'josh', password_digest: 'secret' }
+  context "#new" do
+    should "render ok" do
+      post :new,  :user => {login: 'josh', password_digest: 'secret' }
 
-        assert_response 200
+      assert_response 200
     end
+  end
 
-   end
-    context "#create" do
-      should "create a user account" do
-        assert_difference("User.count", +1) do
-          post :create, :user => {:login => 'josh', :password => 'secret')
-      end
+  context "#create" do
+    should "create a user account" do
+      assert_difference("User.count", +1) do
+        post :create, :user => {:login => 'josh', :password => 'secret'}
+    
         assert User.find_by_login('josh').present?
         assert_redirected_to root_path
       end
+    end
 
     should "not create an account when fields are blank" do
       assert_difference("User.count", 0) do
         post :create, :user => { :login => "Gremlin", :password_digest => "grrr" }
       end
     end
-   end
-
+  end
 end
