bef8d222379573867bd037f70fafd416ee257c10
Christopher Stenqvist <capodacac@gmail.com>
2013-07-22 18:46:26 -0700

    Get password on model working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,17 @@
+require "bcrypt"
+
 class User < ActiveRecord::Base
+  include BCrypt
+
   attr_accessible :login, :password_digest
   validates :login, presence: true
-  #validates :password_digest, presence: true
+  validates :password_digest, presence: true
 
   def password=(str)
+    self.password_digest = Password.create(str).to_s
   end
 
-  def hash(str)
-    BCrypt::Password.create(self.seed + str)
-  end
-
-  private
-  def seed
-    "lkjvneioonvclkljk12394-fuzzy-pickles-ka-ching!"
+  def password
+    @password ||= Password.new(self.password_digest)
   end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -18,12 +18,12 @@ class UserTest < ActiveSupport::TestCase
   context "#password=" do
     should "generate a secure hash for the user" do
       u = User.new
+      u.login = "foo"
       u.password = "it's a secret"
+      u.save!
 
-      assert !u.password_digest.nil?
-      assert_not_equal "it's a secret", u.password_digest
-      assert u.password.nil?
-      assert_equal "$2a$10$Zq9yalZd2YMeaVrgxJKy8Ot5ZFjT2MVkSEKlThQdk5bqH3/pYdNju", u.password_digest
+      u = User.find(u.id)
+      assert u.password == "it's a secret"
     end
   end
 end
