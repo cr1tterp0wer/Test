c24a94e849abaf13fcce63ecc07509620e3d5125
cr1tterp0wer <capodacac@gmail.com>
2013-07-23 23:04:06 -0700

    added test in sessions_create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,13 +9,11 @@ class SessionsController < ApplicationController
   def create
     @temp_pass = params[:password]
     @user = User.find_by_login params[:login]
-    
-    if @user.password == @temp_pass
-      session[:user_id] = @user.id
-      
-    else
-      logged_in = false
-      p "false" * 80
+
+    if @user.present?
+      if @user.password == @temp_pass 
+        session[:user_id] = @user.id
+      end
     end
 
     redirect_to root_path
