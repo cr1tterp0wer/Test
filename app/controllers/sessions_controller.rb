35ff5eac87f9d9e9a21a8b0c7687a7be00779c18
Christopher Stenqvist <capodacac@gmail.com>
2013-07-23 18:22:42 -0700

    prints truefalse

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,8 +12,10 @@ class SessionsController < ApplicationController
     
     if @user.password == @temp_pass
       logged_in = true
+      p "true" * 80
     else
       logged_in = false
+      p "false" * 80
     end
 
     redirect_to root_path
